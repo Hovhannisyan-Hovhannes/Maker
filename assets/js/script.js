2fdf45fa12fa61782bb34279aedb680c91c93f46
Hovhannisyan-Hovhannes <hovhannisyan19081987@gmail.com>
2024-08-09 14:09:20 +0400

    one

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,13 @@ burger.addEventListener('click', () => {
     menuList.classList.toggle('active-menu')
     burger.classList.toggle('active-burger')
 })
+
+$(document).ready(function() {
+    console.log($('.slick-block'))
+    $('.slick-block').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: true,
+    });
+})
